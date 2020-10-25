1c499eace4fefc8de36388131807809c6c16b088
arjunxxx <arjunrajeev1@gmail.com>
2020-10-25 23:37:13 +0530

    Spring Annotation ConstructorInjection Auto wiring

---
+++ 10_SprindAnnotations_ConstructorInjectionAutoWiring/src/com/spring/demo/GymCoach.java
@@ -0,0 +1,33 @@
+package com.spring.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GymCoach implements ICoach {
+
+	
+	private IFortuneService fortuneService;
+	
+	
+	@Autowired
+	public GymCoach(IFortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
+
+
+
+	@Override
+	public String getWorkoutInstruction() {
+		return "do weight trainings now ";
+	}
+
+
+
+	@Override
+	public String getFortuneMessageFromTeller() {
+		return fortuneService.getFortuneMessage();
+	}
+
+}
---
+++ 10_SprindAnnotations_ConstructorInjectionAutoWiring/src/com/spring/demo/ICoach.java
@@ -0,0 +1,8 @@
+package com.spring.demo;
+
+public interface ICoach {
+	
+	public String getWorkoutInstruction();
+	public String getFortuneMessageFromTeller();
+
+}
