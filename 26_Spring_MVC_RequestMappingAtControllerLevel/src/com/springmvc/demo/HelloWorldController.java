d121bdf6b4431d70e8de0793dcf8297dd21cec04
arjunxxx <arjunrajeev1@gmail.com>
2020-10-30 19:40:16 +0530

    changes

---
+++ 26_Spring_MVC_RequestMappingAtControllerLevel/src/com/springmvc/demo/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.springmvc.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("hello")
+public class HelloWorldController {
+	
+	//controller method to show initial form
+	@RequestMapping("/showForm")
+	public String showForm() {		
+		return "helloworld-form";
+		
+	}
+	
+	
+	
+	//controller method top process form
+	
+	@RequestMapping("/processForm")
+	public String processForm(@RequestParam("username") String uName,Model model) {
+		
+		
+		String result="Hey "+ uName.toUpperCase();
+		model.addAttribute("message",result);
+		return "login-page";
+	}
+	
+
+}
